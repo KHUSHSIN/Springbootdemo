5d1bbbea1c0a25081652d33cd71ef3564ce85d70
Awtar Singh <Awtar Singh@192.168.1.39>
2021-08-11 09:54:02 +0530

    added service layer

---
+++ src/main/java/com/onlineCourierManagementApplication/OnlineCourierManagementApplication1Application.java
@@ -0,0 +1,13 @@
+package com.onlineCourierManagementApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineCourierManagementApplication1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineCourierManagementApplication1Application.class, args);
+	}
+
+}
